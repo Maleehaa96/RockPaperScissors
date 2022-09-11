b02465c90cd3430d968a0c8c057ae38663d16449
Maleeha <maleehaa96@gmail.com>
2022-09-11 17:23:42 -0400

    player and computer choices in order UI

--- script.js
+++ script.js
@@ -8,6 +8,104 @@ let winner = false;
 let playerStreak = 0;
 let compStreak = 0;
 
+//score update
+let cScore = document.querySelector('.compScore');
+cScore.textContent = 0;
+
+let pScore = document.querySelector('.playerScore');
+let parent = document.querySelector('.pscore')
+pScore.textContent = 0;
+
+
+
+//button
+let rock = document.querySelector('.rock');
+let paper = document.querySelector('.paper');
+let scissor = document.querySelector('.scissor');
+rock.addEventListener("click",e => Button(e));
+paper.addEventListener("click",e => Button(e));
+scissor.addEventListener("click",e => Button(e));
+
+
+
+// player choice update
+pChoice = document.querySelector('.pChoice');
+let pPrev = document.createElement('div');
+pPrev.textContent =" ";
+pPrev.style.textAlign="center";
+pChoice.append(pPrev);
+
+//comp choice update
+cChoice = document.querySelector('.cChoice');
+let cPrev = document.createElement('div');
+cPrev.textContent = " ";
+cPrev.style.textAlign="center";
+cChoice.append(cPrev);
+
+
+
+
+function Button (e ){
+
+    let response = e.srcElement.className;
+    let playerResponse = getPlayerChoice(response);
+    let computerResponse = getComputerChoice();
+    updatePlayerChoiceUI(playerResponse);
+    updateComputerChoiceUI(computerResponse);
+    decideWinner(playerResponse, computerResponse);
+    
+
+}
+
+
+function updatePlayerChoiceUI(playerChoice){
+   
+    let current = document.createElement('div');
+    switch (playerChoice){
+        case 0:
+            current.textContent = "Rock";
+            break;
+        case 1:
+            current.textContent = "Paper";
+            break;
+        case 2:
+            current.textContent = "Scissor";
+            break;
+     }
+    
+    current.style.textAlign="center";
+    pChoice.insertBefore(current,pPrev);
+    pPrev = current;
+}
+
+function updateComputerChoiceUI(compChoice){
+     
+     let current = document.createElement('div');
+
+     switch (compChoice){
+        case 0:
+            current.textContent = "Rock";
+            break;
+        case 1:
+            current.textContent = "Paper";
+            break;
+        case 2:
+            current.textContent = "Scissor";
+            break;
+     }
+
+     current.style.textAlign = "center";
+    cChoice.insertBefore(current,cPrev);
+    cPrev = current;
+
+     
+}
+
+function updateScoreUI(){
+
+
+}
+
 function getComputerChoice(){
     let result = Math.floor(Math.random() * 3); // results in 0,1,2
     if(result == 0){
@@ -27,40 +125,26 @@ function getComputerChoice(){
     return result;
 }
 
-function getPlayerChoice(){
-       let validResponse = "";
-       let response;
-       
-      while(validResponse != "q"){
-        response = prompt("Enter Rock, Paper, or Scissor");
-        response = response.toLowerCase();
 
+function getPlayerChoice(response){
+       
         if(response =="rock"){
                 console.log("you entered rock");
                 return 0
-            }
-            else if(response == "paper"){
+        }
+        if(response == "paper"){
                 console.log("you entered paper");
                 return 1;
-            }
-            else if (response ==  "scissor"){
+        }
+        if (response ==  "scissor"){
                 console.log("you entered scissor");
                 return 2;
-            }
-            else{
-                alert("Retry. Enter a valid response or press q to quit");
-
-            }
-
-      }
-      
-        
-
+        }
+ 
 }
 
-function decideWinner(){
-    let playerSelection  = getPlayerChoice();
-    let compSelection = getComputerChoice();
+function decideWinner(playerSelection, compSelection){
+    
     if(playerSelection == compSelection){
         alert ("This round is a tie!");
         return;
@@ -138,7 +222,7 @@ function game(){
 }
 
 
-game();
+//game();
 
 //need to keep track of when there is a win
 //use a win 
